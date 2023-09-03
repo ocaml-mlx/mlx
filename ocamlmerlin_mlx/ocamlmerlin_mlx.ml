ca99f4d515a28ff32413d1a9c72a13f8e9c3b623
Andrey Popp <8mayday@gmail.com>
2023-09-03 19:27:18 +0400

    ocamlmerlin_mlx: actually report errors back to merlin
    :facepalm:

--- ocamlmerlin_mlx/ocamlmerlin_mlx.ml
+++ ocamlmerlin_mlx/ocamlmerlin_mlx.ml
@@ -1,5 +1,7 @@
 open Merlin_extend
 open Merlin_extend.Extend_protocol.Reader
+open Ocaml_parsing
+open Mlx_preprocess
 open Mlx_kernel
 
 let parse_string filename str =
@@ -20,11 +22,55 @@ module Mlx_reader = struct
 
   let load buffer = buffer
 
+  let mkstri exp =
+    {
+      Parsetree.pstr_loc = Location.none;
+      pstr_desc = Pstr_eval (exp, []);
+    }
+
+  let mkexp ?(loc = Location.none) pexp_desc =
+    {
+      Parsetree.pexp_loc = loc;
+      pexp_desc;
+      pexp_attributes = [];
+      pexp_loc_stack = [];
+    }
+
+  let mkpayload text =
+    mkstri
+      (mkexp
+         (Pexp_constant
+            (Parsetree.Pconst_string (text, Location.none, None))))
+
+  let to_extension_node exn =
+    match Location.error_of_exn exn with
+    | None -> raise exn
+    | Some `Already_displayed -> None
+    | Some (`Ok error) ->
+        let name =
+          { Location.loc = error.main.loc; txt = "ocaml.error" }
+        in
+        let () = error.main.txt Format.str_formatter in
+        let msg = Format.flush_str_formatter () in
+        let payload = mkpayload msg in
+        Some
+          (mkstri
+             (mkexp ~loc:error.main.loc
+                (Parsetree.Pexp_extension
+                   (name, Parsetree.PStr [ payload ]))))
+
   let parse { text; path; _ } =
     let res = parse_string path text in
+    let parser_errors =
+      List.filter_map to_extension_node res.parser_errors
+    in
+    let lexer_errors =
+      List.filter_map to_extension_node res.lexer_errors
+    in
     match res.parsetree with
     | `Interface intf -> Signature intf
-    | `Implementation impl -> Structure impl
+    | `Implementation impl ->
+        Structure (impl @ parser_errors @ lexer_errors)
 
   let for_completion t _pos = { complete_labels = true }, parse t
 
